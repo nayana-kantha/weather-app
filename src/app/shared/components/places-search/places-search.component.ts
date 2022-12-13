89f304687bee4a8a29f07f3fb913c9c8851b284a
Nayana Kantha <nykkaru@gmail.com>
2022-12-13 14:37:34 +0530

    Create places search component

---
+++ src/app/shared/components/places-search/places-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-places-search',
+  templateUrl: './places-search.component.html',
+  styleUrls: ['./places-search.component.scss']
+})
+export class PlacesSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
