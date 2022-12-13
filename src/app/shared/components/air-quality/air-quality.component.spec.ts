7a1ccba7d1a3aeeb5cf26ba3b6c9f560dc3286a8
Nayana Kantha <nykkaru@gmail.com>
2022-12-13 22:04:50 +0530

    add unit test cases

--- src/app/shared/components/air-quality/air-quality.component.spec.ts
+++ src/app/shared/components/air-quality/air-quality.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ChartModule } from 'primeng/chart';
 
 import { AirQualityComponent } from './air-quality.component';
 
@@ -8,7 +9,8 @@ describe('AirQualityComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AirQualityComponent ]
+      declarations: [ AirQualityComponent ],
+      imports: [ChartModule]
     })
     .compileComponents();
   });
@@ -22,4 +24,26 @@ describe('AirQualityComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('component should have h2 tag', () => {
+    const fixture = TestBed.createComponent(AirQualityComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2')).toBeTruthy();
+  });
+
+  it('component should render "Air Quality" in h2 tag', () => {
+    const fixture = TestBed.createComponent(AirQualityComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Air Quality');
+  });
+
+  it('component should have quality index lable', () => {
+    const fixture = TestBed.createComponent(AirQualityComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.quality-index-lable')).toBeTruthy();
+  });
+
 });
