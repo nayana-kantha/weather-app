a9890fd7b0f0d80054e3927a108aba5a11695d60
nayana-kantha <64595774+nayana-kantha@users.noreply.github.com>
2022-12-14 14:00:41 +0530

    Development (#22)
    * create base structure (#2)
    
    Co-authored-by: Nayana Kantha <nykkaru@gmail.com>
    
    * Feature/base structure (#4)
    
    * create base structure
    
    * base styling add
    
    * fix merge complict
    
    Signed-off-by: nayana-kantha <64595774+nayana-kantha@users.noreply.github.com>
    Co-authored-by: Nayana Kantha <nykkaru@gmail.com>
    
    * Implement header section (#5)
    
    Co-authored-by: Nayana Kantha <nykkaru@gmail.com>
    
    * Feature/header (#6)
    
    * Implement header section
    
    * Add comments
    
    Signed-off-by: nayana-kantha <64595774+nayana-kantha@users.noreply.github.com>
    Co-authored-by: Nayana Kantha <nykkaru@gmail.com>
    
    * Feature/header (#7)
    
    * Implement header section
    
    * Add comments
    
    * add welocome text
    
    Signed-off-by: nayana-kantha <64595774+nayana-kantha@users.noreply.github.com>
    Co-authored-by: Nayana Kantha <nykkaru@gmail.com>
    
    * Feature/header (#8)
    
    * Implement header section
    
    * Add comments
    
    * add welocome text
    
    * Create places search component
    
    Signed-off-by: nayana-kantha <64595774+nayana-kantha@users.noreply.github.com>
    Co-authored-by: Nayana Kantha <nykkaru@gmail.com>
    
    * Feature/header (#9)
    
    * Implement header section
    
    * Add comments
    
    * add welocome text
    
    * Create places search component
    
    * implement google address search
    
    Signed-off-by: nayana-kantha <64595774+nayana-kantha@users.noreply.github.com>
    Co-authored-by: Nayana Kantha <nykkaru@gmail.com>
    
    * Feature/header (#10)
    
    * Implement header section
    
    * Add comments
    
    * add welocome text
    
    * Create places search component
    
    * implement google address search
    
    * add some unit test cases
    
    Signed-off-by: nayana-kantha <64595774+nayana-kantha@users.noreply.github.com>
    Co-authored-by: Nayana Kantha <nykkaru@gmail.com>
    
    * Implement current weather widget (#12)
    
    * Feature/air-quality-widget (#13)
    
    * Implement current weather widget
    
    * Air quality widget implementation
    
    Signed-off-by: nayana-kantha <64595774+nayana-kantha@users.noreply.github.com>
    
    * Feature/air quality widget (#14)
    
    * Implement current weather widget
    
    * Air quality widget implementation
    
    * add unit test cases
    
    Signed-off-by: nayana-kantha <64595774+nayana-kantha@users.noreply.github.com>
    
    * Feature/air quality widget (#15)
    
    * Implement current weather widget
    
    * Air quality widget implementation
    
    * add unit test cases
    
    * dev identified issues fix
    
    Signed-off-by: nayana-kantha <64595774+nayana-kantha@users.noreply.github.com>
    
    * Implement weather forecast widget (#16)
    
    * Feature/weather forecast widget (#17)
    
    * Implement weather forecast widget
    
    * Add error handling
    
    Signed-off-by: nayana-kantha <64595774+nayana-kantha@users.noreply.github.com>
    
    * Feature/weather forecast widget (#18)
    
    * Implement weather forecast widget
    
    * Add error handling
    
    * Imporve folder structure
    
    * update agular cli
    
    * update agular cli
    
    * update primng
    
    * update primng
    
    * update to anugular 15
    
    Signed-off-by: nayana-kantha <64595774+nayana-kantha@users.noreply.github.com>
    
    * Feature/weather forecast widget (#19)
    
    * Implement weather forecast widget
    
    * Add error handling
    
    * Imporve folder structure
    
    * update agular cli
    
    * update agular cli
    
    * update primng
    
    * update primng
    
    * update to anugular 15
    
    * change google search library
    
    Signed-off-by: nayana-kantha <64595774+nayana-kantha@users.noreply.github.com>
    
    * Feature/weather forecast widget (#21)
    
    * Implement weather forecast widget
    
    * Add error handling
    
    * Imporve folder structure
    
    * update agular cli
    
    * update agular cli
    
    * update primng
    
    * update primng
    
    * update to anugular 15
    
    * change google search library
    
    * cross origin issue fix
    
    Signed-off-by: nayana-kantha <64595774+nayana-kantha@users.noreply.github.com>
    
    Signed-off-by: nayana-kantha <64595774+nayana-kantha@users.noreply.github.com>
    Co-authored-by: Nayana Kantha <nykkaru@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   currentWeatherUrl:'https://api.openweathermap.org/data/2.5/weather',
   weatherForcastUrl:'https://api.openweathermap.org/data/2.5/forecast',
-  airPollutionUrl:'http://api.openweathermap.org/data/2.5/air_pollution',
+  airPollutionUrl:'https://api.openweathermap.org/data/2.5/air_pollution',
   id:'524901',
   appid:'be066ec860e6782c1d3be611e6e7a533'
 };
