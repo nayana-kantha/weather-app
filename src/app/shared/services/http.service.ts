ce90f0615a90006868985079c70ff686d40c1203
Nayana Kantha <nykkaru@gmail.com>
2022-12-30 09:19:19 +0530

    api call duplicate issue fixed

--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -6,6 +6,7 @@ import { Location } from '../models/address.model';
 import { AirQuality } from '../models/air-quality.model';
 import { CurrentWeather } from '../models/current-weather.model';
 import { WeatherData } from '../models/weather-data.model';
+import { shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -17,15 +18,21 @@ export class HttpService {
   ) { }
 
   getCurrentWeather(location: Location): Observable<CurrentWeather> {
-    return this.httpClient.get<CurrentWeather>(`${environment.currentWeatherUrl}?id=${environment.id}&appid=${environment.appid}&units=metric&lat=${location.lat}&lon=${location.lng}`);
+    return this.httpClient.get<CurrentWeather>
+      (`${environment.currentWeatherUrl}?id=${environment.id}&appid=${environment.appid}&units=metric&lat=${location.lat}&lon=${location.lng}`)
+      .pipe(shareReplay(1));;
   }
 
   getAirQualityData(location: Location): Observable<AirQuality> {
-    return this.httpClient.get<AirQuality>(`${environment.airPollutionUrl}?appid=${environment.appid}&lat=${location.lat}&lon=${location.lng}`);
+    return this.httpClient.get<AirQuality>
+      (`${environment.airPollutionUrl}?appid=${environment.appid}&lat=${location.lat}&lon=${location.lng}`)
+      .pipe(shareReplay(1));
   }
 
   getWeatherForcastData(location: Location): Observable<WeatherData> {
-    return this.httpClient.get<WeatherData>(`${environment.weatherForcastUrl}?id=${environment.id}&appid=${environment.appid}&cnt=22&units=metric&lat=${location.lat}&lon=${location.lng}`);
+    return this.httpClient.get<WeatherData>
+      (`${environment.weatherForcastUrl}?id=${environment.id}&appid=${environment.appid}&cnt=22&units=metric&lat=${location.lat}&lon=${location.lng}`)
+      .pipe(shareReplay(1));
   }
 
 }
